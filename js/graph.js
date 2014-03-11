ec0578224e5597c9e5a8353b430ee9d53dab446d
Bartunek <gsus0187@gmail.com>
2014-03-11 16:47:37 +0100

    Simple lines drawing
    added methods for drawing lines and reseting whole canvas

--- js/graph.js
+++ js/graph.js
@@ -3,10 +3,51 @@
 Graphie = function(window, id, settings) {
 
 	var self = this,
-		R = window.Raphael,
-		r = R(id, settings.width, settings.height),
-		c = .5,
-		opts = {};
+		R = window.Raphael, // Get Raphael instance
+		r = R(id, settings.width, settings.height), // Create canvas
+		c = .5, // Corection coeficient
+		opts = {}, // configuration object
+		round = function (num, dec) {
+			dec = dec || 2;
+			return  Math.round( num * Math.pow( 10, dec ) ) / Math.pow( 10, dec );
+		},
+		getAnchors = function(p1x, p1y, p2x, p2y, p3x, p3y) {
+			var l1 = (p2x - p1x) / 2,
+				l2 = (p3x - p2x) / 2,
+				a = Math.atan((p2x - p1x) / Math.abs(p2y - p1y)),
+				b = Math.atan((p3x - p2x) / Math.abs(p2y - p3y));
+			a = p1y < p2y ? Math.PI - a : a;
+			b = p3y < p2y ? Math.PI - b : b;
+			var alpha = Math.PI / 2 - ((a + b) % (Math.PI * 2)) / 2,
+				dx1 = l1 * Math.sin(alpha + a),
+				dy1 = l1 * Math.cos(alpha + a),
+				dx2 = l2 * Math.sin(alpha + b),
+				dy2 = l2 * Math.cos(alpha + b);
+			return {
+				x1: p2x - dx1,
+				y1: p2y + dy1,
+				x2: p2x + dx2,
+				y2: p2y + dy2
+			};
+		};
+
+	self._init = function() {
+		self.grid = {};
+		self.xDescItems = [];
+		self.yDescItems = [];
+		self.curves = [];
+		return self;
+	};
+
+
+	// Polyfill for array check
+	if(!Array.isArray) {
+		Array.isArray = function (vArg) {
+			var isArray;
+			isArray = vArg instanceof Array;
+			return isArray;
+		};
+	}
 
 	R.fn.drawGrid = function (x, y, w, h, colCount, rowCount, color) {
 		
@@ -43,40 +84,18 @@ Graphie = function(window, id, settings) {
 		return this.path(path.join(",")).attr({stroke: color});
 	};
 
-	var defaults = {
-		width: 400,
-		height: 400,
-		table: {
-			paddingTop:		20,
-			paddingRight:	0,
-			paddingBottom:	0,
-			paddingLeft:	30,
-			rows:			5,
-			cols:			15,
-			strokeColor:	"#ddd"
-		},
-		xDesc: {
-			text: {
-				font:			"14px Arial",
-				fill:			"#333",
-				"text-anchor":	"middle"
-			}
-		},
-		yDesc: {
-			text: {
-				font:			"14px Arial",
-				fill:			"#333",
-				"text-anchor":	"end"
-			}
-		}
-	};
-
 	self._setDefaults = function(settings, defaults){
 		opts.width =				settings.width || defaults.width;
 		opts.height =				settings.height || defaults.height;
-		opts.table = {};
+		opts.table = {}; opts.xDesc = {}; opts.yDesc = {};
 		if (!settings.table) {
 			settings.table = {};
+		}
+		if (!settings.xDesc) {
+			settings.xDesc = {};
+		}
+		if (!settings.yDesc) {
+			settings.yDesc = {};
 		}
 		opts.table.paddingTop =		settings.table.paddingTop || defaults.table.paddingTop;
 		opts.table.paddingRight =	settings.table.paddingRight || defaults.table.paddingRight;
@@ -85,22 +104,192 @@ Graphie = function(window, id, settings) {
 		opts.table.rows =			settings.table.rows || defaults.table.rows;
 		opts.table.cols =			settings.table.cols || defaults.table.cols;
 		opts.table.strokeColor =	settings.table.strokeColor || defaults.table.strokeColor;
+		opts.table.width =			opts.width - opts.table.paddingLeft - opts.table.paddingRight;
+		opts.table.height =			opts.height - opts.table.paddingTop - opts.table.paddingBottom
 
+		opts.xDesc.text = 			settings.xDesc.text || defaults.xDesc.text;
+		opts.xDesc.dist =			settings.xDesc.dist || defaults.xDesc.dist;
+
+		opts.yDesc.text = 			settings.yDesc.text || defaults.yDesc.text;
+		opts.yDesc.dist =			settings.yDesc.dist || defaults.yDesc.dist;
+
+		opts.line = 				defaults.line;
+		opts.lineHover = 			defaults.lineHover;
 	};
+
+	self._reset = function (settings) {
+		self._setDefaults(settings, self.defaults);
+		r.clear();
+		r.setSize(opts.width, opts.height);
+		return self._init();
+	}
 
 	self._drawGrid = function () {
 		var X = ( ( opts.width - opts.table.paddingLeft - opts.table.paddingRight ) / opts.table.cols ),
 			Y =  ( ( opts.height - opts.table.paddingTop - opts.table.paddingBottom ) / opts.table.rows ),
 			xOrigin = opts.table.paddingLeft,
 			yOrigin = opts.table.paddingTop,
-			tWidth = opts.width - opts.table.paddingLeft - opts.table.paddingRight,
-			tHeight = opts.height - opts.table.paddingTop - opts.table.paddingBottom;
+			tWidth = opts.table.width,
+			tHeight = opts.table.height;
 
 
-		r.drawGrid( xOrigin, yOrigin, tWidth, tHeight, opts.table.cols, opts.table.rows, opts.table.strokeColor);
+		self.grid = r.drawGrid( xOrigin, yOrigin, tWidth, tHeight, opts.table.cols, opts.table.rows, opts.table.strokeColor);
+		return self;
 	};
 
-	self._setDefaults(settings, defaults);
+	self._setHorizontalDesc = function (values, cols) {
+		console.log('Horizontal descriptions', values);
+		cols = Array.isArray(values) ? values.length : opts.table.cols;
+		var stepLength = opts.table.width / ( cols - 1 );
+		if (Array.isArray(values)) {
+			for (var i = 0; i < cols; i++) {
+				self.xDescItems.push(
+					r.text(
+						opts.table.paddingLeft + ( stepLength * i ),
+						opts.table.paddingTop - opts.xDesc.dist,
+						values[i]).attr(opts.xDesc.text)
+				);
+				values[i]
+			};
+		} else if (typeof values === "object") {
+
+		} else {
+
+		}
+		return self;
+	};
+
+	self._setVerticalDesc = function (values, rows) {
+		console.log('Vertical descriptions:', values);
+		rows = Array.isArray(values) ? values.length : opts.table.rows;
+		var stepLength = opts.table.height / ( rows - 1 );
+		
+		if (Array.isArray(values)) {
+			for (var i = 0; i < rows; i++) {
+				if (!values[i]) {continue;}
+				self.yDescItems.push(
+					r.text(
+						opts.table.paddingLeft - opts.yDesc.dist,
+						opts.table.paddingTop + ( stepLength * i ),
+						values[i]).attr(opts.yDesc.text)
+				);
+			};
+		} else if (typeof values === "object") {
+			// TODO Values are given as object of range and number of steps
+		} else {
+			// TODO
+		}
+		return self;
+	};
+
+	self._drawCurve = function (values, style, hoverStyle) {
+		var p = [],
+			xOrig = opts.table.paddingLeft,
+			yOrig = opts.table.paddingTop + ( opts.table.height / 2 ),
+			max = opts.table.height / 2,
+			X = ( ( opts.width - opts.table.paddingLeft - opts.table.paddingRight ) / opts.table.cols ),
+			style = style || opts.line,
+			hStyle = hoverStyle || opts.lineHover;
+
+		for (var i = 0; i < values.length; i++) {
+			var x, x0, x2, y, y0, y1, cp1, cp2, d0X, d0Y, d2X, d2Y, a, coords, line;
+
+			// Actual X coords, next X coords, previous X coords
+			x = round( xOrig + ( X * i ) );
+			x2 = round( xOrig + ( X * (i + 1) ) );
+			x0 = round( xOrig + ( X * (i - 1) ) );
+			x2 = !isNaN(x2) ? x2 : x;
+			x0 = !isNaN(x0) ? x0 : x;
+			
+			// Actual Y coords, next Y coords, previous Y coords
+			y = round( yOrig + ( values[i] * max ) );
+			y2 = round( yOrig + ( values[i + 1] * max ) );
+			y0 = round( yOrig + ( values[i - 1] * max ) );
+			y2 = !isNaN(y2) ? y2 : y;
+			y0 = !isNaN(y0) ? y0 : y;
+
+			// Differences
+			d0X = !isNaN(x0) ? ( x - (x - x0) / 2 ) : x;
+			d0Y = !isNaN(y0) ? ( y - (y - y0) / 2 ) : y;
+			d2X = !isNaN(x2) ? ( x - (x2 - x) / 2 ) : x;
+			d2Y = !isNaN(y2) ? ( y - (y2 - y) / 2 ) : y;
+
+			// console.log('x0:', x0, 'y0:', y0, 'x:', x, 'y:', y, 'x2:', x2, 'y2:', y2);
+			a = getAnchors(x0, y0, x, y, x2, y2);
+
+			coords = [a.x1, a.y1, x, y, a.x2, a.y2];
+			console.log(coords);
+
+			cp1 = a.x1  + "," + a.y1 ;
+			cp2 = a.x2  + "," + a.y2 ;
+
+			// r.circle(d0X, d0Y, 2).attr({fill: "#f00"});
+			// r.circle(d2X, d2Y, 2).attr({fill: "#0f0"});
+
+			if (i === 0) {
+				p.push("M" + x + "," + y );
+				p.push("C" + x + "," + y );
+				// debugger;
+			} else if (i + 1 === values.length) {
+				p.push("L" + x + "," + y );
+			} else {
+				// debugger;
+				p.push("C" + cp1 + "," + cp2 + "," + x + "," + y );
+			}
+		};
+		//console.log('Path:', p.join(''));
+		line = r.path(p.join('')).attr(style);
+		self.curves.push(line);
+		line.hover(
+			function(){
+				this.attr(hStyle);
+			},
+			function(){
+				this.attr(style);
+			}, line, line);
+		return self;
+	}
+
+	self._init();
+	self._setDefaults(settings, self.defaults);
 
 	return self;
+};
+
+Graphie.prototype.defaults = {
+	width: 400,
+	height: 400,
+	table: {
+		paddingTop:		20,
+		paddingRight:	0,
+		paddingBottom:	0,
+		paddingLeft:	30,
+		rows:			5,
+		cols:			15,
+		strokeColor:	"#ddd"
+	},
+	xDesc: {
+		dist:	20,
+		text: {
+			"font":			"14px Arial",
+			"fill":			"#333",
+			"text-anchor":	"middle"
+		}
+	},
+	yDesc: {
+		dist:	10,
+		text: {
+			"font":			"14px Arial",
+			"fill":			"#333",
+			"text-anchor":	"end"
+		}
+	},
+	line: {
+		"stroke": "#aaf",
+		"stroke-opacity": 0.7,
+		"stroke-width": 4,
+	},
+	lineHover : {
+		"stroke-opacity": 1
+	}
 };
--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 
-Bio = function (bday, actdate, range) {
+Bio = function (bday, actdate, accuracy, range) {
 
 	var self = this,
 		now = new Date(Date.now()),
 		dayLength = 86400000,
 		bDay, actDate, daysFromBirth,
+		acc = accuracy || 2,
+		range = range || 1,
 		roundNum = function (num, dec) {
 			return  Math.round( num * Math.pow( 10, dec ) ) / Math.pow( 10, dec );
 		};
@@ -17,7 +19,6 @@ Bio = function (bday, actdate, range) {
 			console.log("Nebylo zadáno datum narození");
 			return false;
 		}
-		range = range || 1;
 		actDate = actdate ? actdate - dayLength : new Date(now.getYear(), now.getMonth(), now.getDate()) - dayLength;
 		daysFromBirth = Math.round( ( actDate - bDay ) / dayLength);
 	};
@@ -35,19 +36,27 @@ Bio = function (bday, actdate, range) {
 			console.log("Nebylo zadáno datum narození");
 			return false;
 		}
-		return this;
+		return self;
 	};
 
-	self.getPhysical = function (days) {
-		return roundNum( 100 * Math.sin( ( ( daysFromBirth % 23 ) * 2 * Math.PI ) / 23 ), 6);
+	self._setAccuracy = function (accuracy) {
+		acc = !isNaN(parseInt(accuracy, 10)) ? parseInt(accuracy) : acc;
+		return self;
 	};
 
-	self.getEmotional = function (days) {
-		return roundNum( 100 * Math.sin( ( ( daysFromBirth % 28 ) * 2 * Math.PI ) / 28 ), 6);
+	self.getPhysical = function ( coef, days) {
+		coef = coef || range;
+		return roundNum( coef * Math.sin( ( ( daysFromBirth % 23 ) * 2 * Math.PI ) / 23 ), acc);
 	};
 
-	self.getIntelectual = function (days) {
-		return roundNum( 100 * Math.sin( ( ( daysFromBirth % 33 ) * 2 * Math.PI ) / 33 ), 6);
+	self.getEmotional = function ( coef, days) {
+		coef = coef || range;
+		return roundNum( coef * Math.sin( ( ( daysFromBirth % 28 ) * 2 * Math.PI ) / 28 ), acc);
+	};
+
+	self.getIntelectual = function ( coef, days) {
+		coef = coef || range;
+		return roundNum( coef * Math.sin( ( ( daysFromBirth % 33 ) * 2 * Math.PI ) / 33 ), acc);
 	};
 
 	console.log(daysFromBirth);
@@ -80,9 +89,9 @@ $(document).ready(function() {
 		console.log("bday:", $input.val(), "actual:", $actual.val());
 		console.log("day:", day, "month:", month, "year:", year);
 
-		$('.js-phy').text(bio.getPhysical());
-		$('.js-emo').text(bio.getEmotional());
-		$('.js-int').text(bio.getIntelectual());
+		$('.js-phy').text(bio.getPhysical(100));
+		$('.js-emo').text(bio.getEmotional(100));
+		$('.js-int').text(bio.getIntelectual(100));
 
 	});
 
@@ -95,9 +104,9 @@ $(document).ready(function() {
 
 		bio.setDate( new Date(actYear, actMonth, actDay) );
 
-		$('.js-phy').text(bio.getPhysical());
-		$('.js-emo').text(bio.getEmotional());
-		$('.js-int').text(bio.getIntelectual());
+		$('.js-phy').text(bio.getPhysical(100));
+		$('.js-emo').text(bio.getEmotional(100));
+		$('.js-int').text(bio.getIntelectual(100));
 
 	});
 
@@ -108,15 +117,76 @@ $(document).ready(function() {
 		height: $('#canvas').innerHeight(),
 		table: {
 			paddingTop: 40,
-			paddingLeft: 40,
+			paddingLeft: 60,
 			paddingRight: 20,
 			paddingBottom: 20,
 			rows: 4,
-			cols: 25
+			cols: 26
+		}
+	},
+	emoStyle = {
+		"stroke": "#fa5ab9",
+		"stroke-opacity": 0.7,
+		"stroke-width": 4
+	},
+	intStyle = {
+		"stroke": "#00cdc3",
+		"stroke-opacity": 0.7,
+		"stroke-width": 4
+	},
+	phyStyle = {
+		"stroke": "#9b81de",
+		"stroke-opacity": 0.7,
+		"stroke-width": 4
+	},
+	yDescriptions = ["100%", "50%", "0%", "-50%", "-100%"],
+	xDescriptions = [],
+	emoVals = [],
+	intVals = [],
+	phyVals = [],
+	dayLength = 86400000,
+	days = settings.table.cols,
+	daysHalf = Math.abs(days / 2);
+
+/*
+	for (var i = 1; i < settings.table.cols + 2; i++) {
+		if ( i > 2 && i < settings.table.cols){
+			xDescriptions.push(i + ".");
+		} else {
+			xDescriptions.push("");
 		}
 	};
+*/
+	bio._setAccuracy(4);
+	for (var i = -(daysHalf), j = 0; i <= daysHalf; i++, j++) {
+		console.log("i:", i, "j:", j);
+		var date = new Date(Date.now() + (i * dayLength)),
+			txt = date.getDate();
+		xDescriptions.push(txt += '.');
+		emoVals.push(bio.setDate(date).getEmotional());
+		intVals.push(bio.getIntelectual());
+		phyVals.push(bio.getPhysical());
+	};
+
+
 	var g = new Graphie(window, "canvas", settings);
 
-	g._drawGrid();
+	g	._drawGrid()
+		._setVerticalDesc(yDescriptions)
+		._setHorizontalDesc(xDescriptions)
+		._drawCurve(emoVals, emoStyle)
+		._drawCurve(intVals, intStyle)
+		._drawCurve(phyVals, phyStyle);
 
+	$(window).on('resize', function(event) {
+		settings.width = $('#canvas').innerWidth();
+		settings.height = $('#canvas').innerHeight();
+		g	._reset(settings)
+			._drawGrid()
+			._setVerticalDesc(yDescriptions)
+			._setHorizontalDesc(xDescriptions)
+			._drawCurve(emoVals, emoStyle)
+			._drawCurve(intVals, intStyle)
+			._drawCurve(phyVals, phyStyle);
+	});
 });
